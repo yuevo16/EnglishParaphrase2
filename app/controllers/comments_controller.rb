0ad14889ab66ed022b86229e22892b08a7c22952
yuya <yuevo16@gmail.com>
2020-02-10 18:14:05 +0900

    comment_controller修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,6 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
     redirect_to themes_path
-    # redirect_to themes_paraphrase_path(:paraphrase_id)
-    # respond_to do |format|
-    #   format.html { redirect_to paraphrase_path(params[:paraphrase_id])  }
-    # #   format.json
-    # end
   end
 
   private
